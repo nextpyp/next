0db66239be44492e5024b6bdaaff585e28d6bba6
Jeff Martin <jeff@cuchazinteractive.com>
2024-04-02 17:26:31 -0400

    don't use Backend.log when checking for Cuda GPUs
    The Cuda GPU checker code gets called during config init, including in the CLI command where
    Backend shouldn't be running, so it causes all kinds of weird errors. Don't do it.

--- src/backendMain/kotlin/edu/duke/bartesaghi/micromon/linux/cuda.kt
+++ src/backendMain/kotlin/edu/duke/bartesaghi/micromon/linux/cuda.kt
@@ -1,11 +1,17 @@
 package edu.duke.bartesaghi.micromon.linux
 
-import edu.duke.bartesaghi.micromon.Backend
 import jcuda.CudaException
 import jcuda.driver.JCudaDriver
+import org.slf4j.LoggerFactory
 
 
 fun countCudaGpus(): Int {
+
+	// WARNING: This function gets called from the CLI, so don't use Backend
+	//          or you will cause weird errors since Backend can't initialize itself correctly in CLI mode.
+
+	// so make a specialized logger here, since we can't use the Backend logger
+	val log = LoggerFactory.getLogger("CudaGPUs")
 
 	try {
 
@@ -17,10 +23,10 @@ fun countCudaGpus(): Int {
 		return ints[0]
 
 	} catch (ex: UnsatisfiedLinkError) {
-		Backend.log.info("CUDA drivers not available, assuming no GPUs")
+		log.info("CUDA drivers not available, assuming no GPUs")
 		return 0
 	} catch (ex: CudaException) {
-		Backend.log.error("Failed to count CUDA GPUs, assuming no GPUs", ex)
+		log.error("Failed to count CUDA GPUs, assuming no GPUs", ex)
 		return 0
 	}
 }
