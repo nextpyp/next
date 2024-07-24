496d1e1c6335b0d7692393529100728bcc01bbf3
jeff <jmartin@cs.duke.edu>
2024-07-24 13:51:06 -0400

    make the segmentation block code to the tomo_segment_close block config, for now

--- src/commonMain/kotlin/edu/duke/bartesaghi/micromon/nodes/TomographySegmentationNodeConfig.kt
+++ src/commonMain/kotlin/edu/duke/bartesaghi/micromon/nodes/TomographySegmentationNodeConfig.kt
@@ -6,7 +6,7 @@ object TomographySegmentationNodeConfig : NodeConfig {
 	const val ID = "tomo-segmentation"
 
 	override val id = ID
-	override val configId = "tomo_segment"
+	override val configId = "tomo_segment_close"
 	override val name = "Segmentation"
 	override val hasFiles = true
 	// TEMP: preview status during development
