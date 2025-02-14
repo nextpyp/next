a3f2ef30d9d8366106c2dc2e76b8b0a6bc48e97c
jeff <jmartin@cs.duke.edu>
2025-02-14 18:11:47 -0500

    fix bugs in websocket connection disconnection/cleanup, so garbage collection can work properly

--- src/frontendMain/kotlin/edu/duke/bartesaghi/micromon/services/WebsocketConnector.kt
+++ src/frontendMain/kotlin/edu/duke/bartesaghi/micromon/services/WebsocketConnector.kt
@@ -86,20 +86,51 @@ class WebsocketConnector(
 
 			// open the websocket connection
 			val socket = Socket()
-			val output = Channel<String>()
-			val input = Channel<String>()
 			try {
 				socket.connect(url)
+			} catch (t: Throwable) {
+				throw Error("can't connect to $path", t)
 			} catch (d: dynamic) {
-				throw Error("can't connect to $path", d)
+				throw Error("can't connect to $path: $d")
 			}
 
-			var outputJob: Job? = null
-			var inputJob: Job? = null
+			// route outgoing messages from the output channel to the websocket connnection
+			val output = Channel<String>()
+			AppScope.launch {
+				for (str in output) {
+					try {
+						socket.send(str)
+					} catch (ex: SocketClosedException) {
+						break
+					}
+				}
+			}
 
-			fun cleanup() {
-				outputJob?.cancel()
-				inputJob?.cancel()
+			// route incoming messages from the websocket connection to the input channel
+			val input = Channel<String>()
+			AppScope.launch {
+				while (true) {
+					try {
+						val str = socket.receive()
+						input.send(str)
+					} catch (ex: SocketClosedException) {
+						break
+					} catch (ex: ClosedSendChannelException) {
+						break
+					}
+				}
+			}
+
+			// start the keep-alive function
+			val keeper = KeeperOfTheAlive(output, input)
+			this@WebsocketConnector.keeper = keeper
+
+			// websocket is live, wire up the disconnector
+			disconnector = {
+				keeper.stop()
+				if (!socket.isClosed()) {
+					socket.close()
+				}
 				if (!output.isClosedForSend) {
 					output.close()
 				}
@@ -108,42 +139,11 @@ class WebsocketConnector(
 				}
 			}
 
-			// route outgoing messages from the output channel to the websocket connnection
-			outputJob = AppScope.launch {
-				for (str in output) {
-					try {
-						socket.send(str)
-					} catch (ex: SocketClosedException) {
-						break
-					}
-				}
-				cleanup()
-			}
-
-			// route incoming messages from the websocket connection to the input channel
-			inputJob = AppScope.launch {
-				while (true) {
-					try {
-						val str = socket.receive()
-						input.send(str)
-					} catch (ex: SocketClosedException) {
-						break
-					}
-				}
-				cleanup()
-			}
-
-			// start the keep-alive function
-			val keeper = KeeperOfTheAlive(output, input)
-			this@WebsocketConnector.keeper = keeper
-
 			// make the signaller for the caller to use
 			val signaler = object : Signaler {
 
 				override fun connected() {
-
-					connectingJob = null
-					disconnector = { output.close() }
+					// just a cosmetic change, really
 					changeState(State.Connected)
 				}
 			}
@@ -154,43 +154,23 @@ class WebsocketConnector(
 				// but send the wrapped channels so we can monitor activity
 				block(signaler, keeper.watchedInput, keeper.watchedOutput)
 
-				// if we get here, the connection exited cleanly
-				disconnector = null
 				changeState(State.Disconnected)
 
 			} catch (ex: CancellationException) {
 
 				// KVision throws this exception to cancel the coroutine when the connection is closed
 				// it's safe to ignore
-
-				// but we're still disconnected
-				disconnector = null
-				changeState(State.Disconnected, null)
+				changeState(State.Disconnected)
 
 			} catch (t: Throwable) {
 
 				// disconnected with an error
-				disconnector = null
 				changeState(State.Disconnected, t)
 			}
 
-			when (state) {
-
-				// if we got here while still connecting, that means the connection attempt failed
-				State.Connecting -> {
-					changeState(State.Disconnected)
-				}
-
-				// someone explicitly tried to abort the connection attempt before it could finish
-				State.Disconnecting -> {
-					changeState(State.Disconnected)
-				}
-
-				// all is well
-				State.Disconnected -> Unit
-
-				else -> console.warn("unexpected connection state", state)
-			}
+			// cleanup, if still needed
+			disconnector?.invoke()
+			disconnector = null
 		}
 	}
 
@@ -290,7 +270,7 @@ class WebsocketConnector(
 			override val onReceiveCatching: SelectClause1<ChannelResult<String>>
 				get() = input.onReceiveCatching
 
-			@Suppress("OverridingDeprecatedMember")
+			@Deprecated("Since 1.2.0, binary compatibility with versions <= 1.1.x", level = DeprecationLevel.HIDDEN)
 			override fun cancel(cause: Throwable?): Boolean =
 				when (cause) {
 					is CancellationException -> {
@@ -397,41 +377,44 @@ class WebsocketConnector(
 			IdleReport(
 				openSeconds(),
 				idleSeconds(),
-				timeoutSeconds
+				TIMEOUT_SECONDS
 			)
 
-		init {
-			AppScope.launch {
+		var task: Job? = AppScope.launch {
 
-				finalIdleReport = null
+			finalIdleReport = null
 
-				// empirically, connections seem to timeout at around 30 seconds of inactivity
-				// so send a ping request periodically
-				// until we reach the desired long-term timeout
-				try {
-					@Suppress("OverridingDeprecatedMember")
-					while (idleSeconds() < timeoutSeconds) {
-						delay(intervalSeconds*1000L)
-						output.sendMessage(RealTimeC2S.Ping())
-					}
-
-					// end of timeout, close the connection
-					output.close()
-
-				} catch (t: Throwable) {
-					// something failed, that usually means the conection closed
-					// so our work here is done
-				} finally {
-					finalIdleReport = idleReport()
+			// empirically, connections seem to timeout at around 30 seconds of inactivity
+			// so send a ping request periodically
+			// until we reach the desired long-term timeout
+			try {
+				@Suppress("OverridingDeprecatedMember")
+				while (idleSeconds() < TIMEOUT_SECONDS) {
+					delay(INTERVAL_SECONDS*1000L)
+					output.sendMessage(RealTimeC2S.Ping())
 				}
+
+				// end of timeout, close the connection
+				disconnect()
+
+			} catch (t: Throwable) {
+				// something failed, that usually means the conection closed
+				// so our work here is done
+			} finally {
+				finalIdleReport = idleReport()
 			}
+		}
+
+		fun stop() {
+			task?.cancel()
+			task = null
 		}
 	}
 
 	companion object {
 		// empirically, connections seem to timeout at around 30 seconds of inactivity
 		// so a ping every 25 seconds or so should keep things working
-		const val intervalSeconds = 25
-		const val timeoutSeconds = 1*60*60 // 1 hour
+		const val INTERVAL_SECONDS = 25
+		const val TIMEOUT_SECONDS = 1*60*60 // 1 hour
 	}
 }
