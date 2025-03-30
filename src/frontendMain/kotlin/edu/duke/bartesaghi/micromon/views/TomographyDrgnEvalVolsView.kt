4b49449918a6c52c131ea2dc48c6d546e24584fd
Alberto Bartesaghi <alberto@cs.duke.edu>
2025-03-29 22:49:36 -0400

    Fix issue in tomodrgn analyze vols block with number of samples along each principal component which is apparently always set to a constant value of 10.

--- src/frontendMain/kotlin/edu/duke/bartesaghi/micromon/views/TomographyDrgnEvalVolsView.kt
+++ src/frontendMain/kotlin/edu/duke/bartesaghi/micromon/views/TomographyDrgnEvalVolsView.kt
@@ -138,20 +138,20 @@ class TomographyDrgnEvalVolsView(val project: ProjectData, val job: TomographyDr
 				}
 
 				addTab("PCA (classes)", "fas fa-ruler-horizontal") { lazyTab ->
-					pcaReconstructionTab = PcaReconstructionTab(evalParams.numClasses).also {
+					pcaReconstructionTab = PcaReconstructionTab(10).also {
 						lazyTab.elem.add(it)
 						lazyTab.onActivate = { it.revalidate() }
 					}
 				}
 
 				addTab("PCA (movie)", "fas fa-film") { lazyTab ->
-					pcaClassesMovieTab = PcaClassesMovieTab(evalParams.numClasses).also {
+					pcaClassesMovieTab = PcaClassesMovieTab(10).also {
 						lazyTab.elem.add(it)
 					}
 				}
 
 				addTab("PCA (3D view)", "fas fa-cube") { lazyTab ->
-					pcaThreeDeeTab = PcaThreeDeeTab(evalParams.numClasses).also {
+					pcaThreeDeeTab = PcaThreeDeeTab(10).also {
 						lazyTab.elem.add(it)
 					}
 				}
